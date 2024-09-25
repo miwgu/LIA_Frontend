9040e77aee48d686697963370d13bb6548e3b602
JonasB91 <jonas.backas@hotmail.com>
2024-09-25 19:58:19 +0200

    #122 Firebase Setup, Adding Firebase config file for our project

---
+++ src/Firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC7WPjL-0wATuPDjg7kAP2oOCZTRMY8PKY",
+  authDomain: "lia-appen.firebaseapp.com",
+  projectId: "lia-appen",
+  storageBucket: "lia-appen.appspot.com",
+  messagingSenderId: "30581706018",
+  appId: "1:30581706018:web:65e09454afa7313a17297b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
