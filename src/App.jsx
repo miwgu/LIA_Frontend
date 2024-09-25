74a76c94148964c4fc178b43964b5b53960a6428
Miwa G <miwa.guhres@hotmail.com>
2024-09-25 11:58:53 +0200

    Test login and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-import { Login, NavBar } from 'fwk-22-a-components';
-import { MockLoginProvider, useMockLogin } from 'fwk-22-a-components';
+//import { Login, NavBar } from 'fwk-22-a-components';
+import NavBar  from 'fwk-22-a-components/src/components/navbar/NavBar';
+import Login  from 'fwk-22-a-components/src/components/Login/Login';
+import {MockLoginProvider} from 'fwk-22-a-components/src/components/Login/MockLoginProvider'
+import { useMockLogin } from 'fwk-22-a-components/src/components/Login/MockLoginProvider';
+//import { MockLoginProvider, useMockLogin } from 'fwk-22-a-components';
+import Home from 'fwk-22-a-components/src/components/HomeTest/Home';
 import './App.css'
 
 function App() {
@@ -8,8 +13,9 @@ function App() {
   return (
     <>
       <NavBar/>
-      <MockLoginProvider>
-        <LoginComponent/>
+       <MockLoginProvider>
+         {/* <LoginComponent/> */} // we do not need this because I add this in MockLoginProvider
+        <Home/>
       </MockLoginProvider>
       
 
@@ -17,10 +23,10 @@ function App() {
   )
 }
 
-const LoginComponent =() =>{
+/* const LoginComponent =() =>{
   const {loginFunction} = useMockLogin ();
   const {navToPage} =useMockLogin();
   return <Login loginFunction={loginFunction} navToPage={navToPage}/>
-} 
+}  */
 
 export default App
