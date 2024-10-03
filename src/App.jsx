24b77137fae43f698503bb8b31449b94431863cc
Miwa G <miwa.guhres@hotmail.com>
2024-10-03 17:24:16 +0200

    Edit App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,92 @@
-//import { Login, NavBar } from 'fwk-22-a-components';
-// import NavBar  from 'fwk-22-a-components/src/components/navbar/NavBar';
-import Login  from 'fwk-22-a-components/src/components/Login/Login';
-import {MockLoginProvider} from 'fwk-22-a-components/src/components/Login/MockLoginProvider'
-import { useMockLogin } from 'fwk-22-a-components/src/components/Login/MockLoginProvider';
-//import { MockLoginProvider, useMockLogin } from 'fwk-22-a-components';
-import Home from 'fwk-22-a-components/src/components/HomeTest/Home';
-import './App.css'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-// import Login from './Components/Login';
-import Registration from './Components/Registration';
+import React, { useEffect } from "react";
+import { PageNavigation } from "./PageNavigation"; 
+import { NavBar,Home, Login, CompanyPage,StudentPage,RegisterCompany,RegisterStudent } from "@miwgu/lia_components";
+import { MockLoginProvider, useMockLogin } from "@miwgu/lia_components";
 
-function App() {
+const App = () => {
+  return (
+    // Wrap everything inside MockLoginProvider first
+    <MockLoginProvider>
+      <AppContent />
+    </MockLoginProvider>
+  );
+};
+
+const AppContent = () => {
+  const { currentPage, navToPage } = PageNavigation();
+  const { user, setUser, loggedIn, setLoggedIn, mockLoginFunc } = useMockLogin(); // Access MockLogin context
+
+  // Role-based navigation logic in useEffect
+  useEffect(() => {
+    const loggedInUser = JSON.parse(localStorage.getItem('loggedInUserData'));
+     const currentPath = window.location.pathname;
+     console.log("currentPath: ", currentPath)
+    if (currentPath) {
+      navToPage(currentPath.replace("/", ""));
+    } 
+    /**
+     * loggedInUser-> the app found in user from localStrage
+     * !loggedIn -> not yet merked the user as loggedIn
+     * -->Update the login state this prevent unnecessary update
+     * to the state if the user is already logged in.
+     */
+
+    if (loggedInUser && !loggedIn) {
+      setUser(loggedInUser);
+      setLoggedIn(true);
+
+      if (loggedInUser.role === 'student') {
+        navToPage('companyPage');
+        
+      } else if (loggedInUser.role === 'company') {
+        navToPage('studentPage');
+      
+      }  
+    } else if(!loggedInUser) {
+      navToPage('login');
+    } 
+  }, [loggedIn,navToPage, setUser, setLoggedIn]);
+
   return (
     <>
-      {/* <NavBar/> */}
-       <MockLoginProvider>
-         {/* <LoginComponent/> */} // we do not need this because I add this in MockLoginProvider
-        <Home/>
-      </MockLoginProvider>
-
-      <BrowserRouter>
-        <nav>
-          {/* <Link to="/Login">Logga in</Link> */}
-          <br />
-          <Link to="/Registration">Registrera</Link>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<h1>Välkommen till startsidan</h1>} />
-          {/* <Route path="/Login" element={<Login />} /> */}
-          <Route path="/Registration" element={<Registration />} />
-        </Routes>
-      </BrowserRouter>
+      <NavBarWrapper navToPage={navToPage} />
+      <MainAppContent currentPage={currentPage} navToPage={navToPage} />
     </>
-  )
-}
+  );
+};
 
-/* const LoginComponent =() =>{
-const LoginComponent =() =>{
-  const {loginFunction} = useMockLogin ();
-  const {navToPage} =useMockLogin();
-  return <Login loginFunction={loginFunction} navToPage={navToPage}/>
-}  */
+const NavBarWrapper = ({ navToPage }) => {
+  const { loggedIn } = useMockLogin();
+  return <NavBar loggedIn={loggedIn} navToPage={navToPage} />;
+};
 
+const MainAppContent = ({ currentPage }) => {
+  const { mockLoginFunc, navToPage } = useMockLogin(); // Access the login function
 
-export default App
+  const renderCurrentPage = () => {
+    switch (currentPage) {
+      case "companyPage":
+        return <CompanyPage />;
+      case "studentPage":
+        return <StudentPage />;
+      case "registerStudent":
+        return <RegisterStudent />;
+      case "registerCompany":
+        return <RegisterCompany />;
+      case "login":
+        return (
+        <Login 
+          loginFunction={(email, password)=> mockLoginFunc(email, password, navToPage)} 
+          navToPage={navToPage} 
+        />
+      );
+      case "home":
+      default:
+        return <Home />;
+    }
+  };
+
+  return <div>{renderCurrentPage()}</div>;
+};
+
+export default App;
